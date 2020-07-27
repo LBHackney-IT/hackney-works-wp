266cf4c72635a3f8ac4135d0e340ac8b603a40cb
Jaye Hackett <jayehackett@FutureGovs-MacBook-Pro.local>
2020-07-27 21:36:04 +0100

    add copyright notice

--- footer.php
+++ footer.php
@@ -1,10 +1,9 @@
 <footer class="site-footer">
     <div class="site-footer__navigation">
 
-        <div class="container">
+        <div class="container site-footer__columns">
             <img src="<?php echo get_stylesheet_directory_uri() . "/assets/footer-logo.png" ?>" alt="Hackney Council"/>
             
-
             <?php wp_nav_menu(array(
                 "theme_location" => "footer-left-menu",
                 "menu_class" => "site-footer__menu",
@@ -23,7 +22,7 @@
     </div>
     <div class="site-footer__copyright">
         <div class="container">
-            <p>Copyright message here</p>
+            <p>All content is available under the <a href="https://www.nationalarchives.gov.uk/doc/open-government-licence/version/3/">Open Government Licence v3.0</a>, except where otherwise stated</p>
         </div>
     </div>
 </footer>

31b9d55fb428d22811842a517245741f3f2b4975
Jaye Hackett <jayehackett@FutureGovs-MacBook-Pro.local>
2020-07-27 22:24:27 +0100

    connect button to custom field

--- index.php
+++ index.php
@@ -3,17 +3,26 @@
 <?php if(have_posts()): while(have_posts()): the_post(); ?>
 
 <section class="hero <?php if(has_post_thumbnail()){ echo "hero--with-image"; } ?>">
-    <?php if(has_post_thumbnail()): ?>
+    
+<?php if(has_post_thumbnail()): ?>
         <div class="hero__background" style="background-image: url('<?php echo get_the_post_thumbnail_url( null, "full" ); ?>')"></div>
     <?php endif; ?>
+
     <div class="hero__content">
         <h1 class="hero__title"><?php the_title(); ?></h1>
         <?php if(has_excerpt()): ?>
             <p class="hero__excerpt"><?php echo get_the_excerpt(); ?></p>
         <?php endif; ?>
 
-        <a href="#" class="button hero__call-to-action">Find a course</a>
-
+        <?php if(get_field("call_to_action")): ?>
+            <a href="<?php get_the_permalink(get_field("call_to_action")) ?>" class="button hero__call-to-action">
+                <?php if(get_field("call_to_action_text")){
+                    echo get_field("call_to_action_text");
+                } else {
+                    echo get_the_title(get_field("call_to_action"));
+                } ?>
+            </a>
+        <?php endif; ?>
     </div>
 </section>
 

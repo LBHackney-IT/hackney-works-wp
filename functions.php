bab16d20c9638e79f190e3ccbacf082be94ec7be
Jaye Hackett <jayehackett@FutureGovs-MacBook-Pro.local>
2020-10-18 16:08:13 +0100

    use fix api key

--- functions.php
+++ functions.php
@@ -45,7 +45,7 @@ add_action( "init", "lbh_register_menus" );
 
 // Configure ACF Google Maps field
 function lbh_acf_init() {
-    acf_update_setting('google_api_key', GOOGLE_API_KEY);
+    acf_update_setting('google_api_key', GOOGLE_CLIENT_KEY);
 }
 add_action('acf/init', 'lbh_acf_init');
 

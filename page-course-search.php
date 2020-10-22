7507b9f878ebd0c80c115ee091c83745130bb93f
Jaye Hackett <jayehackett@FutureGovs-MacBook-Pro.local>
2020-10-22 11:52:32 +0100

    fix course search page

--- page-course-search.php
+++ page-course-search.php
@@ -55,8 +55,8 @@ get_header();
 if(have_posts()): while(have_posts()): the_post(); ?>
 
 <section class="hero hero--with-image hero--course-search">
-    <img class="hero__lines" src="<?php echo get_template_directory_uri(); ?>/assets/lines-inverted.svg" alt=""/>
     <div class="hero__background" style="background-image: url('<?php echo get_the_post_thumbnail_url( null, "full" ); ?>')"></div>
+    <img class="hero__lines" src="<?php echo get_template_directory_uri(); ?>/assets/lines-inverted.svg" alt=""/>
     <div class="hero__content">
         <h1 class="hero__title"><?php the_title(); ?></h1>
     </div>

d0b9b6c42eaaae86cb559e09247893f9a04ab248
Jaye Hackett <jayehackett@FutureGovs-MacBook-Pro.local>
2020-11-02 11:26:45 +0000

    move featured opps to top

--- page-promotional.php
+++ page-promotional.php
@@ -42,20 +42,11 @@ if(have_posts()): while(have_posts()): the_post(); ?>
 
 <article class="page-content page-content--deeper-padding">
     <?php get_template_part("announcement"); ?>
-    
-    <div class="content-area container container--narrow">
-        <?php the_content(); ?>
-    </div>
-</article>
 
-<?php endwhile; else: ?>
 
-<p>Nothing to show</p>
-
-<?php endif; ?>
 
 <?php if(is_front_page() && $featured_opps->have_posts()): ?>
-    <section class="page-content similar-courses">
+    <section class="similar-courses similar-courses--padding-top">
         <div class="container container--mid">
             <h2 class="similar-courses__title">Featured opportunities</h2>
             
@@ -86,6 +77,18 @@ if(have_posts()): while(have_posts()): the_post(); ?>
         </div>
 
     </section>
+<?php endif; ?>
+
+
+    <div class="content-area container container--narrow">
+        <?php the_content(); ?>
+    </div>
+</article>
+
+<?php endwhile; else: ?>
+
+<p>Nothing to show</p>
+
 <?php endif; ?>
 
 <?php get_template_part("testimonials"); ?>
